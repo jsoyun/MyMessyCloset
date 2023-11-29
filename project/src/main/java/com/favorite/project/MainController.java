615d53a99fa70ce40ee162f4bdf2c66bd7ada6db
soyun <syusmm233@gmail.com>
2023-11-30 00:40:45 +0900

    Change return Json Data

--- project/src/main/java/com/favorite/project/MainController.java
+++ project/src/main/java/com/favorite/project/MainController.java
@@ -1,14 +1,23 @@
 package com.favorite.project;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
 
 @Controller
+
 public class MainController {
     @GetMapping("/index")
     public String index(){
         return "/index";
     }
+
+
     @GetMapping("/login")
     public String login(){
         return "/login";
---
+++ project/src/main/java/com/favorite/project/model/UserData.java
@@ -0,0 +1,12 @@
+package com.favorite.project.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class UserData {
+    private String name;
+    private int age;
+
+}
