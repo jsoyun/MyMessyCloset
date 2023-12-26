87e3208544b362ee5313ec33088b0fc291faca83
jsoyun <syusmm233@gmail.com>
2023-12-26 14:32:11 +0900

    add:mybatis-setting

--- project/src/main/java/com/favorite/project/config/MariaDBConfig.java
+++ project/src/main/java/com/favorite/project/config/MariaDBConfig.java
@@ -2,24 +2,26 @@ package com.favorite.project.config;
 
 import com.zaxxer.hikari.HikariConfig;
 import com.zaxxer.hikari.HikariDataSource;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.mybatis.spring.SqlSessionFactoryBean;
+import org.mybatis.spring.SqlSessionTemplate;
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
-import org.springframework.context.ApplicationContext;
+
 
 import javax.sql.DataSource;
 
 @Configuration
-//@PropertySource("classpath:/application.yml")
+@MapperScan(basePackages = "com.favorite.project.mapper")
+@PropertySource("classpath:/application.yml")
 public class MariaDBConfig {
 
-//    @Autowired
-//    private ApplicationContext applicationContext;
 
     @Bean
-    @ConfigurationProperties(prefix="spring.datasource.hikari")
+    @ConfigurationProperties(prefix = "spring.datasource.hikari")
     public HikariConfig hikariConfig() {
         return new HikariConfig();
     }
@@ -27,7 +29,21 @@ public class MariaDBConfig {
     @Bean
     public DataSource dataSource() throws Exception {
         DataSource dataSource = new HikariDataSource(hikariConfig());
-        System.out.println(dataSource.toString());
+        System.out.println("dataSource 메서드 안" + dataSource.toString() + "dataSource 메서드 안");
         return dataSource;
+    }
+
+    @Bean
+    public SqlSessionFactory sqlSessionFactory() throws Exception {
+        SqlSessionFactoryBean factoryBean = new SqlSessionFactoryBean();
+        System.out.println("factoryBean = " + factoryBean);
+        factoryBean.setDataSource(dataSource());
+        factoryBean.setTypeAliasesPackage("com.favorite.project.mapper");
+        return factoryBean.getObject();
+    }
+
+    @Bean
+    public SqlSessionTemplate sqlSession(SqlSessionFactory sqlSessionFactory) {
+        return new SqlSessionTemplate(sqlSessionFactory);
     }
 }
