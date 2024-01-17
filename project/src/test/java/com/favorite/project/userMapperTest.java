868c6c06a4490c0cb3060514d87c8b8d278fa821
jsoyun <syusmm233@gmail.com>
2024-01-17 14:04:19 +0900

    add:userMapperTest

---
+++ project/src/test/java/com/favorite/project/userMapperTest.java
@@ -0,0 +1,111 @@
+package com.favorite.project;
+
+import com.favorite.project.domain.entity.User;
+import com.favorite.project.mapper.UserMapper;
+import lombok.extern.slf4j.Slf4j;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+//import org.mybatis.spring.boot.test.autoconfigure.MybatisTest;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+@Slf4j
+//@MybatisTest
+public class userMapperTest {
+    @Mock
+    private UserMapper userMapper;
+
+    @BeforeEach
+    public void setUser() {
+        MockitoAnnotations.openMocks(this); //?
+
+    }
+
+    @Test
+    public void GetAllUsersTest() {
+        //given
+        List<User> userList = new ArrayList<>();
+        User mockTest = User.builder().userId(1L).email("김생선").password("1234").build();
+        User mockTest2 = User.builder().userId(2L).email("이선생").password("4321").build();
+        userList.add(mockTest);
+        userList.add(mockTest2);
+        when(userMapper.getAllUsers()).thenReturn(userList);
+
+        //when
+        List<User> userList1 = userMapper.getAllUsers();
+
+        //then
+        verify(userMapper).getAllUsers();
+        Assertions.assertThat(userList1.stream().filter(e -> e.getName().equals("김생선")));
+
+        //log
+        userList1.stream().peek(user -> {
+
+            System.out.println("user.getUserId() = " + user.getUserId());
+            System.out.println("user.getEmail() = " + user.getEmail());
+            System.out.println("user.getPassword() = " + user.getPassword());
+        }).forEach(user -> {
+            log.info("리스트={}");
+        });
+
+
+    }
+
+
+    @Test
+    public void GetUserByEmailTest() {
+        //given
+        User mocktest = User.builder().email("Mocktest").password("1234").build();
+        when(userMapper.getByEmail("Mocktest")).thenReturn(mocktest);
+
+        //when
+        User UserByEmail = userMapper.getByEmail("Mocktest");
+
+        //then
+        verify(userMapper).getByEmail("Mocktest");
+        Assertions.assertThat(UserByEmail.getEmail()).isEqualTo("Mocktest");
+        Assertions.assertThat(UserByEmail.getPassword()).isEqualTo("1234");
+
+        //log
+        log.info("getUser :" + UserByEmail.getEmail() + ", " + UserByEmail.getPassword() + ", ");
+    }
+
+    @Test
+    public void InsertUserTest() {
+        //given
+        User mocktest = User.builder().email("user1").password("1234").build();
+        when(userMapper.insert(mocktest)).thenReturn(true);
+        //when
+        boolean result = userMapper.insert(mocktest);
+        //then
+        verify(userMapper).insert(mocktest);
+        Assertions.assertThat(result);
+
+    }
+
+    @Test
+    public void GetByIdTest() {
+        //given
+        User mocktest = User.builder().userId(1L).email("user1").password("1234").build();
+        when(userMapper.getById(1L)).thenReturn(mocktest);
+        //when
+        User UserbyId = userMapper.getById(1L);
+        //then
+        verify(userMapper).getById(1L);
+        Assertions.assertThat(mocktest.getEmail().equals(UserbyId.getEmail()));
+        Assertions.assertThat(UserbyId);
+
+        log.info("user ={},{},{}", UserbyId.getUserId(), UserbyId.getEmail(), UserbyId.getPassword());
+
+    }
+
+
+}
