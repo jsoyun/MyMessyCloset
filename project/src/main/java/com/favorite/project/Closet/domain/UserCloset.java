2f163753aaea18da1445eb9875919f56e215dfc0
jsoyun <syusmm233@gmail.com>
2024-01-19 07:14:09 +0900

    Add: UserCloset domain , deleted userClosetId in User domain

--- project/src/main/java/com/favorite/project/Closet/domain/UserCloset.java
+++ project/src/main/java/com/favorite/project/Closet/domain/UserCloset.java
@@ -8,9 +8,10 @@ import lombok.Getter;
 @Builder
 public class UserCloset {
     private int id; //id값은 autoincrement로 자동으로 추가되는데 builder에서 제외하는게 좋을까?
+    private String name;
     private String color;
     private String notes;
-    private int clothes_id;
     private int user_id;
+
 
 }
--- project/src/main/java/com/favorite/project/User/domain/User.java
+++ project/src/main/java/com/favorite/project/User/domain/User.java
@@ -10,7 +10,6 @@ public class User {
     private String email;
     private String name;
     private String password;
-    private Integer userClosetId;
 
 
 }
