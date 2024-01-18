9cd3ac21040705986db3bf53e547a44635eceb18
jsoyun <syusmm233@gmail.com>
2024-01-19 07:16:22 +0900

    Add: ClosetMapper, ClosetMapperTest

---
+++ project/src/main/java/com/favorite/project/Closet/Mapper/ClosetMapper.java
@@ -0,0 +1,21 @@
+package com.favorite.project.Closet.Mapper;
+
+import com.favorite.project.Closet.domain.UserCloset;
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Select;
+
+import java.util.List;
+
+@Mapper
+public interface ClosetMapper {
+    //TODO: 필요한 값만 선택해서 가져올 수 있도록.dto 이용해서
+    @Select("SELECT * FROM userCloset")
+    List<UserCloset> getAllUserClosets();
+
+
+    @Insert("INSERT INTO userCloset (name, color, notes,user_id) VALUES (#{UserCloset.name},#{UserCloset.color},#{UserCloset.notes},#{UserCloset.user_id})")
+    boolean insertUserCloset(@Param("UserCloset") UserCloset closet);
+
+}
