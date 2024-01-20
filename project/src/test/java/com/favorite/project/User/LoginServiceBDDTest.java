4d132b841a2452a2b427fe9e3ff0ce3dbc83ef7f
jsoyun <syusmm233@gmail.com>
2024-01-20 22:33:01 +0900

    Add: LoginServiceTest and LoginServiceBDDTest

--- project/src/test/java/com/favorite/project/User/LoginServiceBDDTest.java
+++ project/src/test/java/com/favorite/project/User/LoginServiceBDDTest.java
@@ -1,10 +1,7 @@
 package com.favorite.project.User;
 
-
-import com.favorite.project.User.dto.LoginForm;
 import com.favorite.project.User.domain.User;
-import lombok.RequiredArgsConstructor;
-import org.assertj.core.api.Assertions;
+import com.favorite.project.User.dto.LoginForm;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -12,18 +9,15 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 
-import java.util.NoSuchElementException;
 import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.verify;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.BDDMockito.then;
 import static org.mockito.Mockito.when;
 
-
-public class LoginServiceTest {
-
+public class LoginServiceBDDTest {
 
     @Mock
     private LoginUserService loginUserService;
@@ -41,11 +35,11 @@ public class LoginServiceTest {
     }
 
     @Test
-    @DisplayName("checkLoginForm메서드 : 유저의 이메일이 맞고 이메일의 비밀번호도 맞다")
+    @DisplayName("checkLoginForm테스트")
     void checkLoginFormTest() {
         //given
         //loginUserService가 email에 맞는 user값을 반환했다고 가정하면
-        when(loginUserService.getUserByEmail(any())).thenReturn(Optional.of(user));
+        given(loginUserService.getUserByEmail(any(User.class))).willReturn(Optional.of(user));
 
         //when
         //checkLoginForm 를 호출했을 때
@@ -57,31 +51,7 @@ public class LoginServiceTest {
         assertThat(result.get().getEmail().equals("so@naver.com"));
         assertThat(result.get().getPassword().equals("1234"));
 
-//        org.junit.jupiter.api.Assertions.assertNotEquals(result.);
-
-
-    }
-
-    //TODO: 예외도 처리해야함
-    @Test
-    @DisplayName("loginUserService.getUserByEmail값이 없을 때 checkLoginForm 실패")
-    void uncheckLoginFormTest() {
-
-        //given
-        when(loginUserService.getUserByEmail(any())).thenReturn(Optional.empty());
-
-        User userEmail = User.builder().email("test@test").build();
-
-        //when
-//        Optional<User> result = loginService.checkLoginForm(loginForm); //실행시, java.util.NoSuchElementException: User not found
-
-        //then
-
-        assertThrows(NoSuchElementException.class, () ->
-                loginService.checkLoginForm(loginForm)
-
-        );
-
+        then(loginUserService).should().getUserByEmail(any(User.class));
 
     }
 
