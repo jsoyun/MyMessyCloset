c3522e3ff195bc29e2c8db23becb36568e10507b
jsoyun <syusmm233@gmail.com>
2024-01-24 17:47:14 +0900

    Fix: add GlobalExceptionHandler - NoSuchElementException,MethodArgumentNotValidException

---
+++ project/src/main/java/com/favorite/project/exception/GlobalExceptionHandler.java
@@ -0,0 +1,40 @@
+package com.favorite.project.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.NoSuchElementException;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    @ResponseStatus(value = HttpStatus.BAD_REQUEST)
+    public Object processValidationError(MethodArgumentNotValidException exception) {
+        List<String> errorMessages = new ArrayList<>();
+        exception.getBindingResult().getFieldErrors().forEach((error) -> {
+            String errorMessage = error.getField() + ": " + error.getDefaultMessage();
+            errorMessages.add(errorMessage);
+        });
+
+
+        ErrorResponse elementNotFound = ErrorResponse.builder().message("유효한 값이 아닙니다").errors(errorMessages).status(HttpStatus.BAD_REQUEST).build();
+        return new ResponseEntity<>(elementNotFound, elementNotFound.getStatus());
+
+    }
+
+
+    @ExceptionHandler(NoSuchElementException.class)
+    @ResponseStatus(value = HttpStatus.NOT_FOUND) //명시적 선언, 하지만 ErrorResponse의 status세부 설정이 우선권 가진다.
+    public ResponseEntity<ErrorResponse> handleNoSuchElementException(NoSuchElementException exception) {
+        ErrorResponse elementNotFound = ErrorResponse.builder().message(exception.getMessage()).status(HttpStatus.NOT_FOUND).build();
+        return new ResponseEntity<>(elementNotFound, elementNotFound.getStatus());
+
+    }
+
+
+}
