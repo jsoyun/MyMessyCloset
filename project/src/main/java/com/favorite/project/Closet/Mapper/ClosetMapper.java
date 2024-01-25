99c1244add4f00d64f706a4330e268f1c00bbea8
jsoyun <syusmm233@gmail.com>
2024-01-25 22:36:55 +0900

    Fix: delete  @Select("SELECT * FROM userCloset") -> add ClosetMapper.xml

--- project/src/main/java/com/favorite/project/Closet/Mapper/ClosetMapper.java
+++ project/src/main/java/com/favorite/project/Closet/Mapper/ClosetMapper.java
@@ -1,6 +1,7 @@
 package com.favorite.project.Closet.Mapper;
 
 import com.favorite.project.Closet.domain.UserCloset;
+import com.favorite.project.User.domain.User;
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
@@ -11,11 +12,13 @@ import java.util.List;
 @Mapper
 public interface ClosetMapper {
     //TODO: 필요한 값만 선택해서 가져올 수 있도록.dto 이용해서
-    @Select("SELECT * FROM userCloset")
-    List<UserCloset> getAllUserClosets();
+//    @Select("SELECT * FROM userCloset")
+//    List<UserCloset> getAllUserClosets();
 
 
     @Insert("INSERT INTO userCloset (name, color, notes,user_id) VALUES (#{UserCloset.name},#{UserCloset.color},#{UserCloset.notes},#{UserCloset.user_id})")
     boolean insertUserCloset(@Param("UserCloset") UserCloset closet);
+
+    List<UserCloset> getAllUserClosets();
 
 }
