594307962809440d0224f73ba1d32f1d38f48f6e
jsoyun <syusmm233@gmail.com>
2024-02-07 14:39:20 +0900

    Add: SQLException, BindException, PersistenceException

--- project/src/main/java/com/favorite/project/exception/ExceptionResponse.java
+++ project/src/main/java/com/favorite/project/exception/ExceptionResponse.java
@@ -6,6 +6,12 @@ import org.springframework.http.HttpStatus;
 
 import java.util.List;
 
+/**
+ * status : HTTP 상태코드
+ * exceptions : 예외에 대한 세부정보
+ * message: 전반적인 예외 설명, 사용자에게 보여줄 메시지
+ */
+
 @Builder
 @Getter
 public class ExceptionResponse {
