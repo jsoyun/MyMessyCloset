6bd7bd046fd249d1eb686d567f95948e384c7c71
jsoyun <syusmm233@gmail.com>
2024-02-12 21:11:54 +0900

    Feature: ClothesResponseDto to return

---
+++ project/src/main/java/com/favorite/project/Clothes/dto/ClothesResponseDto.java
@@ -0,0 +1,22 @@
+package com.favorite.project.Clothes.dto;
+
+import com.favorite.project.Clothes.SeasonType;
+import com.favorite.project.ClothesCategory.ClothesCategoryEnumType;
+import lombok.Builder;
+import lombok.Getter;
+
+import java.time.LocalDate;
+
+@Builder
+@Getter
+public class ClothesResponseDto {
+    private ClothesCategoryEnumType clothesCategoryType;
+    private SeasonType seasonType;
+    private int price;
+    private String imgUrl;
+    private LocalDate purchasedDate;
+    private int userId;
+    private int categoryId;
+    private int userClosetId;
+
+}
