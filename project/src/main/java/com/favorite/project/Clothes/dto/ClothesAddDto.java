fed664295bf45d560fe258990b97ff2bf3d4b579
jsoyun <syusmm233@gmail.com>
2024-02-12 21:04:01 +0900

    Feature: ClothesAddDto

---
+++ project/src/main/java/com/favorite/project/Clothes/dto/ClothesAddDto.java
@@ -0,0 +1,34 @@
+package com.favorite.project.Clothes.dto;
+
+import com.favorite.project.Closet.domain.UserCloset;
+import com.favorite.project.Clothes.domain.Clothes;
+import com.favorite.project.ClothesCategory.ClothesCategoryEnumType;
+import com.favorite.project.Clothes.SeasonType;
+import lombok.Builder;
+import lombok.Getter;
+
+import java.time.LocalDate;
+
+@Builder
+@Getter
+public class ClothesAddDto {
+    private ClothesCategoryEnumType clothesCategoryType;
+    private SeasonType seasonType;
+    private int price;
+    private String imgUrl;
+    private LocalDate purchasedDate;
+    private int userClosetId;
+
+
+    public Clothes toClothes(ClothesAddDto clothesAddDto) {
+        return Clothes.builder().clothesCategoryEnumType(clothesAddDto.clothesCategoryType)
+                .seasonType(clothesAddDto.seasonType)
+                .price(clothesAddDto.price)
+                .imgUrl(clothesAddDto.imgUrl)
+                .purchasedDate(clothesAddDto.purchasedDate)
+                .userClosetId(clothesAddDto.userClosetId)
+                .build();
+    }
+
+
+}
