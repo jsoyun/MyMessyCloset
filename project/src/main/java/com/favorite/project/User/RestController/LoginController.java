d186d4a47fcbfc7e9e59cceec995884f40f56e51
jsoyun <syusmm233@gmail.com>
2024-02-20 08:03:51 +0900

    Add: remove cookie when session invalidate

--- project/src/main/java/com/favorite/project/User/RestController/LoginController.java
+++ project/src/main/java/com/favorite/project/User/RestController/LoginController.java
@@ -4,15 +4,17 @@ import com.favorite.project.User.LoginService;
 import com.favorite.project.User.domain.User;
 import com.favorite.project.User.dto.LoginRequestDTO;
 import com.favorite.project.User.dto.LoginResponseDTO;
-import com.favorite.project.Validator.SessionManager;
+import com.favorite.project.Validator.SessionConst;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import javax.validation.Valid;
 
 @Slf4j
@@ -22,26 +24,38 @@ import javax.validation.Valid;
 public class LoginController {
 
     private final LoginService loginService;
-    private final SessionManager sessionManager;
 
     @PostMapping("/login")
-    public ResponseEntity<LoginResponseDTO> login(@RequestBody @Valid LoginRequestDTO loginRequestDTO, HttpServletResponse response) {
+    public ResponseEntity<LoginResponseDTO> login(@RequestBody @Valid LoginRequestDTO loginRequestDTO, HttpServletRequest request, HttpServletResponse response) {
 
         LoginResponseDTO loginResponseDTO = loginService.checkLoginForm(loginRequestDTO);
-        //로그인 성공시 sessionId 생성 (password는 없음)
         User user = loginResponseDTO.toUser(loginResponseDTO);
-        sessionManager.createSession(user, response);
+        //기본은 true, 없으면 새로운 세션을 생성한다.
+        HttpSession session = request.getSession();
+        //세션에 데이터 저장
+        session.setAttribute(SessionConst.USER_SESSION_ID, user);
         return ResponseEntity.ok(loginResponseDTO);
-
-
-//TODO: 일치하는 회원정보가 없을 때 메인화면으로 리다이렉트
-
     }
 
     @GetMapping("/logout")
     public ModelAndView logout(HttpServletRequest request, HttpServletResponse response) {
-        System.out.println("logout = ");
-        sessionManager.expire(request, response);
+        HttpSession session = request.getSession(false);
+        if (session != null) {
+            //세션 삭제
+            session.invalidate();
+        }
+        //쿠키 삭제
+        Cookie[] cookies = request.getCookies();
+        if (cookies != null) {
+            for (Cookie cookie : cookies) {
+                if (cookie.getName().equals("JSESSIONID")) {
+                    cookie.setMaxAge(0);
+                    response.addCookie(cookie);
+                    break;
+                }
+            }
+
+        }
         return new ModelAndView("redirect:/");
 
 
