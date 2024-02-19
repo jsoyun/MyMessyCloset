315a57437358db1ec815269966832c9b6563b7a2
jsoyun <syusmm233@gmail.com>
2024-02-19 16:02:29 +0900

    Add: toUser Method from LoginResponseDTO in LoginResponseDTO

--- project/src/main/java/com/favorite/project/User/dto/LoginResponseDTO.java
+++ project/src/main/java/com/favorite/project/User/dto/LoginResponseDTO.java
@@ -1,5 +1,6 @@
 package com.favorite.project.User.dto;
 
+import com.favorite.project.User.domain.User;
 import lombok.Builder;
 import lombok.Getter;
 
@@ -11,6 +12,15 @@ public class LoginResponseDTO {
     private Long id;
     private String name;
     private String email;
+
+    public User toUser(LoginResponseDTO loginResponseDTO) {
+        return User.builder()
+                .id(loginResponseDTO.id)
+                .email(loginResponseDTO.email)
+                .name(loginResponseDTO.name)
+                .build();
+
+    }
 
 
 }
