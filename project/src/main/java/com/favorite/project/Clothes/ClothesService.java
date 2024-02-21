900386e98d14902df663421ede24d3e6e7aa69a2
jsoyun <syusmm233@gmail.com>
2024-02-22 08:59:57 +0900

    Add:checkUserCloset in ClothesService

--- project/src/main/java/com/favorite/project/Clothes/ClothesService.java
+++ project/src/main/java/com/favorite/project/Clothes/ClothesService.java
@@ -6,9 +6,9 @@ import com.favorite.project.Clothes.dto.ClothesAddDto;
 import com.favorite.project.Clothes.dto.ClothesResponseDto;
 import com.favorite.project.ClothesCategory.ClothesCategoryEnumType;
 import com.favorite.project.ClothesCategory.ClothesCategoryService;
+import com.favorite.project.UserCloset.UserClosetValidator;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.ibatis.exceptions.PersistenceException;
 import org.springframework.stereotype.Service;
 
 @Slf4j
@@ -16,27 +16,33 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class ClothesService {
     private final ClothesMapper clothesMapper;
+    private final UserClosetValidator closetValidator;
 
     private final ClothesCategoryService clothesCategoryService;
 
     public ClothesResponseDto addClothes(ClothesAddDto clothesAddDto) {
         Clothes clothes = clothesAddDto.toClothes(clothesAddDto);
-        ClothesCategoryEnumType categoryEnumType = clothes.getCategoryName();
-        int clothesCategoryId = clothesCategoryService.getClothesCategoryId(categoryEnumType);
-        clothes.setCategoryId(clothesCategoryId);
-        boolean result = clothesMapper.insertClothes(clothes);
-        ClothesResponseDto clothesResponseDto;
-        if (result) {
-            clothesResponseDto = clothes.toClothesResponseDto(clothes);
+        boolean checkValidClosetResult = checkValidCloset(clothes);
+
+        if (checkValidClosetResult) {
+
+            ClothesCategoryEnumType categoryEnumType = clothes.getCategoryName();
+            int clothesCategoryId = clothesCategoryService.getClothesCategoryId(categoryEnumType);
+            clothes.setCategoryId(clothesCategoryId);
+            clothesMapper.insertClothes(clothes);
+            return clothes.toClothesResponseDto(clothes);
+
         } else {
-            throw new PersistenceException("옷 저장에 실패했습니다.");
+            throw new IllegalArgumentException("유효한 옷장이 아닙니다");
 
         }
 
 
-        return clothesResponseDto;
+    }
 
+    public boolean checkValidCloset(Clothes clothes) {
 
+        return closetValidator.checkUserCloset(clothes.getUserClosetId());
     }
 
 
