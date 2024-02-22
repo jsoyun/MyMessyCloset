8f276296ce774655b2b152cb5f9b7f6538e86c24
jsoyun <syusmm233@gmail.com>
2024-02-22 09:00:53 +0900

    Fix: check session in addClothes

--- project/src/main/java/com/favorite/project/Clothes/ClothesController.java
+++ project/src/main/java/com/favorite/project/Clothes/ClothesController.java
@@ -2,27 +2,40 @@ package com.favorite.project.Clothes;
 
 import com.favorite.project.Clothes.dto.ClothesAddDto;
 import com.favorite.project.Clothes.dto.ClothesResponseDto;
+import com.favorite.project.User.domain.User;
+import com.favorite.project.UserCloset.UserClosetValidator;
+import com.favorite.project.Validator.SessionConst;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 
 @RestController
 @RequiredArgsConstructor
 public class ClothesController {
 
     private final ClothesService clothesService;
+    private final UserClosetValidator userClosetValidator;
 
 
     @PostMapping("/clothes")
-    public ResponseEntity<ClothesResponseDto> addClothes(@RequestBody ClothesAddDto clothesAddDto) {
-        //TODO: 옷장이 유효한지 검증, 유요한 옷타입 카테고리인지 검증
-        ClothesResponseDto clothesResponseDto = clothesService.addClothes(clothesAddDto);
-        return new ResponseEntity<>(clothesResponseDto, HttpStatus.OK);
+    public ResponseEntity<ClothesResponseDto> addClothes(@RequestBody ClothesAddDto clothesAddDto, HttpServletRequest request) {
+        // TODO: 유효한 옷타입 카테고리인지 검증
 
+        HttpSession session = request.getSession(false);
+        if (session != null) {
+            User user = (User) session.getAttribute(SessionConst.USER_SESSION_ID);
+            userClosetValidator.checkUserBySession(user.getId());
+            ClothesResponseDto clothesResponseDto = clothesService.addClothes(clothesAddDto);
+            return new ResponseEntity<>(clothesResponseDto, HttpStatus.OK);
 
+        } else {
+            throw new NullPointerException("세션 정보가 없습니다.");
+        }
     }
+
 
 }
