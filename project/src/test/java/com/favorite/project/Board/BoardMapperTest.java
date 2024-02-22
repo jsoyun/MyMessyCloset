e1d79761125e2f4ca03c8e6c2736425934f59c95
jsoyun <syusmm233@gmail.com>
2024-02-22 18:08:47 +0900

    Feature: InsertBoardSuccessTest in BoardMapperTest

---
+++ project/src/test/java/com/favorite/project/Board/BoardMapperTest.java
@@ -0,0 +1,48 @@
+package com.favorite.project.Board;
+
+import com.favorite.project.Board.Mapper.BoardMapper;
+import com.favorite.project.Board.domain.Board;
+import com.favorite.project.Board.domain.BoardStatus;
+import com.favorite.project.Board.domain.BoardType;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.time.LocalDateTime;
+
+import static org.mockito.BDDMockito.given;
+
+@ExtendWith(MockitoExtension.class)
+public class BoardMapperTest {
+
+    @Mock
+    private BoardMapper boardMapper;
+
+    @Test
+    @DisplayName("게시판 추가하기 성공")
+    public void InsertBoardSuccessTest() {
+        Board board = Board.builder()
+                .createdAt(LocalDateTime.now())
+                .boardType(BoardType.RENT)
+                .clothesId(123)
+                .price(50.0)
+                .rentalHours(4)
+                .content("This is a sample content.")
+                .imageUrl("https://example.com/image.jpg")
+                .location("Sample Location")
+                .status(BoardStatus.AVAILABLE)
+                .build();
+
+        //given
+        given(boardMapper.insertBoard(board)).willReturn(true);
+        //when
+        boolean result = boardMapper.insertBoard(board);
+        //then
+        Assertions.assertThat(result).isEqualTo(true);
+
+
+    }
+}
