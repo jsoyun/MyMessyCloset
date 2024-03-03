d42f7f6a07970a25e74ffb7ee68f92c5ea515f87
jsoyun <syusmm233@gmail.com>
2024-03-04 06:38:23 +0900

    Fix: return Board when insertBoard

--- project/src/main/java/com/favorite/project/Board/Mapper/BoardMapper.java
+++ project/src/main/java/com/favorite/project/Board/Mapper/BoardMapper.java
@@ -9,5 +9,5 @@ import org.apache.ibatis.annotations.Param;
 public interface BoardMapper {
 
     @Insert("INSERT INTO Board (createdAt,boardType, clothesId, price, rentalHours, content, imageUrl, location,status) VALUES (#{board.createdAt},#{board.boardType}, #{board.clothesId}, #{board.price} , #{board.rentalHours},#{board.content}, #{board.imageUrl}, #{board.location}, #{board.status} )")
-    boolean insertBoard(@Param("board") Board board);
+    Board insertBoard(@Param("board") Board board);
 }
