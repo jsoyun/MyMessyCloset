414345ec7b21ae3ecdc99922d7c0579da8c5d23d
jsoyun <syusmm233@gmail.com>
2024-03-14 00:32:01 +0900

    Feature: use ClothesListDto when mapping sql query

---
+++ project/src/main/java/com/favorite/project/Clothes/dto/ClothesListDto.java
@@ -0,0 +1,21 @@
+package com.favorite.project.Clothes.dto;
+
+import com.favorite.project.Clothes.SeasonType;
+import lombok.Builder;
+import lombok.Getter;
+
+import java.time.LocalDate;
+
+@Builder
+@Getter
+public class ClothesListDto {
+    private int id;
+    private int userClosetId;
+    private int clothesCategoryEnumTypeId;
+    private int price;
+    private String img;
+    private LocalDate purchasedDate;
+    private SeasonType season;
+
+
+}
