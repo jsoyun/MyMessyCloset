59995bdf55a5678db9a0afa3eb5e685c2dea3ab4
jsoyun <syusmm233@gmail.com>
2024-03-14 00:35:09 +0900

    Fix: Add MyBatis mapper methods(selectAllClothes,selectClothesBySeason) for fetching ClothesListDto by userClosetId and season

--- project/src/main/java/com/favorite/project/Clothes/Mapper/ClothesMapper.java
+++ project/src/main/java/com/favorite/project/Clothes/Mapper/ClothesMapper.java
@@ -1,14 +1,28 @@
 package com.favorite.project.Clothes.Mapper;
 
+import com.favorite.project.Clothes.SeasonType;
 import com.favorite.project.Clothes.domain.Clothes;
-import org.apache.ibatis.annotations.Insert;
-import org.apache.ibatis.annotations.Mapper;
-import org.apache.ibatis.annotations.Param;
+import com.favorite.project.Clothes.dto.ClothesListDto;
+import org.apache.ibatis.annotations.*;
+
+import java.util.List;
 
 @Mapper
 public interface ClothesMapper {
     @Insert("INSERT INTO Clothes (clothes_category_id,season, price,img,purchased_date,userCloset_id) VALUES (#{Clothes.categoryId},#{Clothes.seasonType},#{Clothes.price},#{Clothes.imgUrl},#{Clothes.purchasedDate},#{Clothes.userClosetId})")
     boolean insertClothes(@Param("Clothes") Clothes clothes);
+
+    @Results({
+            @Result(property = "clothesCategoryEnumTypeId", column = "clothes_category_id"),
+            @Result(property = "purchasedDate", column = "purchased_date"),
+            @Result(property = "userClosetId", column = "userCloset_id")
+    })
+    @Select("SELECT id,userCloset_id,clothes_category_id, price,img, purchased_date,season  FROM Clothes WHERE userCloset_id = #{UserClosetId}")
+    List<ClothesListDto> selectAllClothes(@Param("UserClosetId") int userClosetId);
+
+
+    @Select("SELECT id,userCloset_id,clothes_category_id, price,img, purchased_date,season FROM Clothes WHERE season = #{seasonType} ")
+    List<ClothesListDto> selectClothesBySeason(@Param("seasonType") SeasonType seasonType);
 
 
 }
