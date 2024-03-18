a3546ce506f0515dc0c898ec4ceb108ac09b0177
jsoyun <syusmm233@gmail.com>
2024-03-19 01:18:57 +0900

    Fix: renamed getAllClothesByUserClosetId

--- project/src/main/java/com/favorite/project/Clothes/Mapper/ClothesMapper.java
+++ project/src/main/java/com/favorite/project/Clothes/Mapper/ClothesMapper.java
@@ -18,7 +18,7 @@ public interface ClothesMapper {
             @Result(property = "userClosetId", column = "userCloset_id")
     })
     @Select("SELECT id,userCloset_id,clothes_category_id, price,img, purchased_date,season  FROM Clothes WHERE userCloset_id = #{UserClosetId}")
-    List<ClothesListDto> selectAllClothes(@Param("UserClosetId") int userClosetId);
+    List<ClothesListDto> selectAllClothesById(@Param("UserClosetId") int userClosetId);
 
 
     @Select("SELECT id,userCloset_id,clothes_category_id, price,img, purchased_date,season FROM Clothes WHERE season = #{seasonType} ")
