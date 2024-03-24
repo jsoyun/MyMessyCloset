957043ef3fdbb5207aaea80b102ca0fe71ba35b0
jsoyun <syusmm233@gmail.com>
2024-03-24 16:58:10 +0900

    Add: successGetClothes By userClosetId Test Not using Mock

--- project/src/test/java/com/favorite/project/Clothes/ClothesControllerTest.java
+++ project/src/test/java/com/favorite/project/Clothes/ClothesControllerTest.java
@@ -1,6 +1,7 @@
 package com.favorite.project.Clothes;
 
 import com.favorite.project.Clothes.dto.ClothesAddDto;
+import com.favorite.project.Clothes.dto.ClothesListDto;
 import com.favorite.project.ClothesCategory.ClothesCategoryEnumType;
 import com.favorite.project.UserCloset.UserClosetValidator;
 import org.junit.jupiter.api.Assertions;
@@ -13,8 +14,12 @@ import org.mockito.junit.jupiter.MockitoExtension;
 
 import javax.servlet.http.HttpServletRequest;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.mock;
+import static org.assertj.core.api.Assertions.assertThat;
 
 @ExtendWith(MockitoExtension.class)
 public class ClothesControllerTest {
@@ -38,6 +43,31 @@ public class ClothesControllerTest {
         //when
         //then
         Assertions.assertThrows(NullPointerException.class, () -> clothesController.addClothes(ClothesAddDto.builder().build(), request));
+
+    }
+
+    @Test
+    @DisplayName("GET_CLOTHES_BY_USERCLOSETID")
+    public void successGetClothes() {
+
+        //결과값
+        List<ClothesListDto> ClothesList = new ArrayList<>();
+        ClothesListDto build = ClothesListDto.builder().userClosetId(54).clothesCategoryEnumTypeId(1).build();
+        ClothesListDto build2 = ClothesListDto.builder().userClosetId(54).clothesCategoryEnumTypeId(2).build();
+        ClothesList.add(build);
+        ClothesList.add(build2);
+
+
+        int userClosetId = 54;
+        int page = 1;
+        int pageSize = 2;
+
+
+        for (int i = page - 1; i < pageSize; i++) {
+            assertThat(ClothesList.get(i).getUserClosetId()).isEqualTo(userClosetId);
+        }
+        assertThat(ClothesList.size()).isEqualTo(pageSize);
+
 
     }
 }
