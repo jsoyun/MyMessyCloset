70dca8b60118cc233c027689cb51c6c746db7804
jsoyun <syusmm233@gmail.com>
2024-03-26 20:45:32 +0900

    Add: postWrongClothesId in BoardServiceTest, fixed BoardMapperTest

--- project/src/test/java/com/favorite/project/Board/BoardServiceTest.java
+++ project/src/test/java/com/favorite/project/Board/BoardServiceTest.java
@@ -1,12 +1,9 @@
 package com.favorite.project.Board;
 
 import com.favorite.project.Board.Mapper.BoardMapper;
-import com.favorite.project.Board.domain.Board;
-import com.favorite.project.Board.domain.BoardStatus;
-import com.favorite.project.Board.domain.BoardType;
 import com.favorite.project.Board.dto.BoardAddDto;
-import com.favorite.project.Board.dto.BoardResponseDto;
-import org.assertj.core.api.Assertions;
+import com.favorite.project.Clothes.Mapper.ClothesMapper;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -14,44 +11,29 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
-import java.time.LocalDateTime;
+import java.util.NoSuchElementException;
 
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 public class BoardServiceTest {
 
     @Mock
-    private BoardMapper boardMapper;
+    private ClothesMapper clothesMapper;
     @InjectMocks
     private BoardService boardService;
 
-    @Test
-    @DisplayName("게시판 올리기 성공")
-    public void PostBoardSuccess() {
-//
-        BoardAddDto boardAddDto = BoardAddDto.builder()
-                .createdAt(LocalDateTime.now())
-                .boardType(BoardType.RENT)
-                .clothesId(123)
-                .price(50.0)
-                .rentalHours(4)
-                .content("This is a sample content.")
-                .imageUrl("https://example.com/image.jpg")
-                .location("Sample Location")
-                .status(BoardStatus.AVAILABLE)
-                .build();
 
-        Board board = boardAddDto.toBoard(boardAddDto);
-        //given
-        given(boardMapper.insertBoard(any(Board.class))).willReturn(board);
-        //when
-        BoardResponseDto boardResponseDto = boardService.PostBoard(boardAddDto);
-        //then
-        Assertions.assertThat(boardMapper.insertBoard(board)).isEqualTo(board);
-        Assertions.assertThat(boardResponseDto.getLocation()).isEqualTo(boardAddDto.getLocation());
+    @Test
+    @DisplayName("잘못된_옷_정보를_게시판에_올려서_실패")
+    public void postWrongClothesId() {
+
+        BoardAddDto boardAddDto = BoardAddDto.builder().clothesId(1).build();
+        when(clothesMapper.checkClothesById(boardAddDto.getClothesId())).thenReturn(false);
+
+        Assertions.assertThrows(NoSuchElementException.class, () -> boardService.PostBoard(boardAddDto));
 
     }
+
 
 }
