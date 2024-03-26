5fb08d21dd0f0776b929ef6a99b47811fa3269f8
jsoyun <syusmm233@gmail.com>
2024-03-26 20:43:48 +0900

    Add: BoardService

--- project/src/main/java/com/favorite/project/Board/BoardService.java
+++ project/src/main/java/com/favorite/project/Board/BoardService.java
@@ -4,20 +4,31 @@ import com.favorite.project.Board.Mapper.BoardMapper;
 import com.favorite.project.Board.domain.Board;
 import com.favorite.project.Board.dto.BoardAddDto;
 import com.favorite.project.Board.dto.BoardResponseDto;
+import com.favorite.project.Clothes.Mapper.ClothesMapper;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.NoSuchElementException;
 
 @Service
 @RequiredArgsConstructor
 public class BoardService {
     private final BoardMapper boardMapper;
+    private final ClothesMapper clothesMapper;
 
-    //TODO: 판매 또는 대여 게시판 올리기
 
+    //하나의 옷만 올릴 수 있음.
     public BoardResponseDto PostBoard(BoardAddDto boardAddDto) {
+        boolean clothes = clothesMapper.checkClothesById(boardAddDto.getClothesId());
+        if (!clothes) {
+            throw new NoSuchElementException("존재하는 옷이 아닙니다.");
+
+
+        }
         Board board = boardAddDto.toBoard(boardAddDto);
-        Board createdBoard = boardMapper.insertBoard(board);
-        return BoardResponseDto.builder().build().toBoardResponse(createdBoard);
+        boardMapper.insertBoard(board);
+
+        return BoardResponseDto.builder().build().toBoardResponse(board);
 
 
     }
