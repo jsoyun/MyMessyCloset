9b1feefc3866037463069c14c9cdb46ef6c06f49
jsoyun <syusmm233@gmail.com>
2024-03-26 20:21:16 +0900

    Add: userId,clothesId and use Setter

--- project/src/main/java/com/favorite/project/Board/domain/Board.java
+++ project/src/main/java/com/favorite/project/Board/domain/Board.java
@@ -2,20 +2,27 @@ package com.favorite.project.Board.domain;
 
 import lombok.Builder;
 import lombok.Getter;
+import lombok.Setter;
 
+import javax.validation.constraints.NotNull;
 import java.time.LocalDateTime;
 
 @Builder
 @Getter
 public class Board {
     private LocalDateTime createdAt;
-    private BoardType boardType;
+    @NotNull
+    @Setter
+    private Long userId;
+    private BoardType boardType; //RENT일 경우 시간 필수기재 , SALE retalHours가 limit 무한사용가능
+    @NotNull
+    @Setter
     private int clothesId;
     private double price;
     private int rentalHours;
     private String content;
     private String imageUrl;
     private String location;
-    private BoardStatus status;
+    private BoardStatus status; //기본적으로 예약가능  AVAILABLE 이다
 
 }
